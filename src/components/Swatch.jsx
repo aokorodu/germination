81a556890a0795f904ec3b591e0011bec5b4ca62
aokorodu <aokorodu@gmail.com>
2024-03-09 19:30:54 -0500

    near done

--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -1,11 +1,15 @@
+import styles from "./Swatch.module.scss";
+
 const Swatch = ({ color, callback }) => {
   return (
     <div
+      className={styles.swatch}
       onClick={() => {
         callback(color);
       }}
+      style={{ background: `${color}` }}
     >
-      <svg width="30" height="30" viewBox="0 0 30 30">
+      {/* <svg width="30" height="30" viewBox="0 0 30 30">
         <rect
           x="0"
           y="0"
@@ -17,7 +21,7 @@ const Swatch = ({ color, callback }) => {
           rx={10}
           ry={10}
         />
-      </svg>
+      </svg> */}
     </div>
   );
 };
